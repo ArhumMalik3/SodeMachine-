82953970c3cf91af7edf4a58b1f3e004b2f43052
ArhumMalik3 <ArhumMalik95@gmail.com>
2020-08-19 15:29:12 -0500

    I added DisplayWelcome, DisplaySodaOptions, and ChooseSoda static methods in the Userinterface class.

--- SodaMachine/Userinterface.cs
+++ SodaMachine/Userinterface.cs
@@ -8,5 +8,28 @@ namespace SodaMachine
 {
     public static class Userinterface
     {
+
+        //member variables
+
+        //properties
+
+        //constructor
+
+
+        //member methods
+        public static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the Soda Machine");
+        }
+
+        public static void DisplaySodaOptions()
+        {
+
+        }
+
+        public static void ChooseSoda()
+        {
+
+        }
     }
 }
